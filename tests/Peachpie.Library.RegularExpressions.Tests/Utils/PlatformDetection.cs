11bec9f7931727cec4c3b3c9fb02400a03c3db8e
Robert Husák <robert@husak.cloud>
2019-10-01 15:25:42 +0200

    Add tests from CoreFX modified for PCRE
    Non-compilable ones were commented out (mostly because of the unsupported RemoteExecutor), failing are being skipped.

---
+++ tests/Peachpie.Library.RegularExpressions.Tests/Utils/PlatformDetection.cs
@@ -0,0 +1,40 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics;
+using System.IO;
+using System.Security;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Runtime.CompilerServices;
+using Microsoft.Win32;
+using Xunit;
+
+namespace System
+{
+    public static class PlatformDetection
+    {
+        private static volatile Tuple<bool> s_lazyNonZeroLowerBoundArraySupported;
+        public static bool IsNonZeroLowerBoundArraySupported
+        {
+            get
+            {
+                if (s_lazyNonZeroLowerBoundArraySupported == null)
+                {
+                    bool nonZeroLowerBoundArraysSupported = false;
+                    try
+                    {
+                        Array.CreateInstance(typeof(int), new int[] { 5 }, new int[] { 5 });
+                        nonZeroLowerBoundArraysSupported = true;
+                    }
+                    catch (PlatformNotSupportedException)
+                    {
+                    }
+                    s_lazyNonZeroLowerBoundArraySupported = Tuple.Create<bool>(nonZeroLowerBoundArraysSupported);
+                }
+                return s_lazyNonZeroLowerBoundArraySupported.Item1;
+            }
+        }
+    }
+}
---
+++ tests/Peachpie.Library.RegularExpressions.Tests/Utils/SkipHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Peachpie.Library.RegularExpressions.Tests
+{
+    /// <summary>
+    /// Helper class to enable selective test skipping and forcing it to run.
+    /// </summary>
+    internal static class SkipHelper
+    {
+        /// <summary>
+        /// Constant to add in the front of a test pattern to cause its skipping using <see cref="SkipIfMarked(ref string)"/>.
+        /// The reason should be mentioned in a comment.
+        /// </summary>
+        public const string SkippedPattern = "SKIP_THIS_";
+
+        /// <summary>
+        /// Constant to add in the front of a test pattern to cause its skipping using <see cref="SkipIfMarked(ref string)"/>.
+        /// The reason is the different handling of named groups, such as (?<123>foo) or (?'abcd'bar).
+        /// </summary>
+        public const string SkippedPatternNamedGroup = SkippedPattern;
+
+        /// <summary>
+        /// The environment variable marking to ignore skipping and run all tests.
+        /// </summary>
+        public const string ForceSkippedEnvironmentVariable = "PEACHPIE_PERLREGEX_TESTS_FORCE_SKIPPED";
+        public const string ForceSkippedEnvironmentVariableValue = "1";
+
+        /// <summary>
+        /// Skip the test if the pattern starts with <see cref="SkippedPattern"/> unless the environment variable
+        /// <see cref="ForceSkippedEnvironmentVariable"/> is set to <see cref="ForceSkippedEnvironmentVariableValue"/>.
+        /// </summary>
+        public static void SkipIfMarked(ref string pattern)
+        {
+            if (pattern.StartsWith(SkippedPattern))
+            {
+                if (Environment.GetEnvironmentVariable(ForceSkippedEnvironmentVariable) == ForceSkippedEnvironmentVariableValue)
+                    pattern = pattern.Substring(SkippedPattern.Length);
+                else
+                    Skip.If(true);
+            }
+        }
+    }
+}
