52da8197d9c6f3fec14ce995dac79ae3a177dda7
Robert Husák <robert@husak.cloud>
2019-11-11 19:28:45 +0100

    Remove large variant of RegexRedux benchmark

--- tests/Peachpie.Library.RegularExpressions.Benchmarks/Program.cs
+++ tests/Peachpie.Library.RegularExpressions.Benchmarks/Program.cs
@@ -47,44 +47,6 @@ namespace Peachpie.Library.RegularExpressions.Benchmarks
         public RegexRedux.Result Pcre() => RegexRedux.RunPcre(_sequences);
     }
 
-    [Config(typeof(Config))]
-    [MemoryDiagnoser]
-    public class ReduxBenchmarkSlow
-    {
-        private class Config : ManualConfig
-        {
-            public Config()
-            {
-                Add(Job.Default
-                    .With(CoreRuntime.Core30)
-                    .WithLaunchCount(1)
-                    .WithWarmupCount(1)
-                    .WithInvocationCount(1)
-                    .WithUnrollFactor(1)
-                    .WithIterationCount(3));
-
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    Add(new EtwProfiler());
-                }
-            }
-        }
-
-        private string _sequences;
-
-        [GlobalSetup]
-        public void Setup()
-        {
-            _sequences = File.ReadAllText("regexredux-input5000000.txt");
-        }
-
-        [Benchmark(Baseline = true)]
-        public RegexRedux.Result CoreFx() => RegexRedux.RunCoreFx(_sequences);
-
-        [Benchmark]
-        public RegexRedux.Result Pcre() => RegexRedux.RunPcre(_sequences);
-    }
-
     class Program
     {
         static void Main()
